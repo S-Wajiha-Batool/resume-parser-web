c60890831316a5cdbb6c7d93ad550a5d73b063c0
hamcmd <mohammad.hammad@dealcart.io>
2023-05-26 04:08:54 +0500

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.REACT_APP_TARGET,
+      target: "https://resume-parser-web-backend.onrender.com",
       changeOrigin: true,
     })
   );
